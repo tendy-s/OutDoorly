7ad96ce74f02df5513596bb9e85e62aba8be32a4
Tendy Su <tendysu@gmail.com>
2023-05-30 20:02:10 -0700

    updates park details page

--- front-end/src/pages/park-details/index.js
+++ front-end/src/pages/park-details/index.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { getParkDetails } from "../../services/park-service";
 import { Tab, Tabs } from "@mui/material";
 import { TabPanel } from "../../components/ParkDetailsTabPanel";
+import PhotosAndReviews from "../../components/PhotosAndReviews";
 
 export default function ParkDetails() {
   const { parkCode } = useParams();
@@ -28,15 +29,20 @@ export default function ParkDetails() {
 
   return (
     <div className={styles.parkDetailsWrapper}>
+      <h2>{parkDetails.fullName}</h2>
+      <img
+        className={styles.mainImg}
+        src="https://images.unsplash.com/photo-1498429089284-41f8cf3ffd39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80"
+      />
       {/* {JSON.stringify(parkDetails)} */}
       <Tabs
         value={value}
         onChange={handleChange}
         aria-label="basic tabs example"
       >
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+        <Tab label="Description" />
+        <Tab label="Operating Hours" />
+        <Tab label="Weather Info" />
       </Tabs>
       <TabPanel value={value} index={0}>
         {parkDetails.description}
@@ -47,6 +53,7 @@ export default function ParkDetails() {
       <TabPanel value={value} index={2}>
         {parkDetails.weatherInfo}
       </TabPanel>
+      <PhotosAndReviews />
     </div>
   );
 }
