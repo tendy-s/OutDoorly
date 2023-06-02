b2ca57bbec7addccb69f934871e72d11135b2b51
Babak <hadadybabak@gmail.com>
2023-06-01 20:29:45 -0700

    finished ReviewsModal skeleton

---
+++ front-end/src/components/Modal/PhotosModal/index.js
@@ -0,0 +1,5 @@
+// TODO: modal for user to upload image
+
+export default function PhotosModal() {
+    return <div></div>
+}
---
+++ front-end/src/components/Modal/ReviewsModal/index.js
@@ -0,0 +1,74 @@
+import Backdrop from "@mui/material/Backdrop";
+import {
+  Modal,
+  Fade,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Rating,
+} from "@mui/material";
+import { useState } from "react";
+import styles from "./reviews-modal.module.scss";
+
+export default function ReviewsModal(props) {
+  const setVisible = props.setVisible;
+  const [open, setOpen] = useState(true);
+  const [rating, setRating] = useState(0);
+  function handleClose() {
+    setVisible(false);
+  }
+
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 500,
+        },
+      }}>
+      <Fade in={open}>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+          }}
+          className={styles.modalBox}>
+          <Typography
+            className={styles.modalHeader}
+            id="transition-modal-title"
+            variant="h6"
+            component="h2">
+            Add a Review
+          </Typography>
+          <TextField
+            className={styles.modalTextField}
+            margin={"small"}
+            id="outlined-multiline-static"
+            label="Review Description"
+            multiline
+            rows={3}
+          />
+          <Rating
+            className={styles.modalRating}
+            name="no-value"
+            value={rating}
+            onChange={(event, newValue) => {
+              setRating(newValue);
+            }}
+          />
+          <Button
+            onClick={() => {
+              setVisible(false);
+            }}>
+            Submit
+          </Button>
+        </Box>
+      </Fade>
+    </Modal>
+  );
+}
