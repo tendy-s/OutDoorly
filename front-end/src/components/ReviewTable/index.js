a964026c45d1e17d5a67ffc0f3684a88fb726241
Tendy Su <tendysu@gmail.com>
2023-06-04 15:53:06 -0700

    minor changes to navBar

--- front-end/src/components/ReviewTable/index.js
+++ front-end/src/components/ReviewTable/index.js
@@ -5,38 +5,49 @@ import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
+import NaturePeopleIcon from "@mui/icons-material/NaturePeople";
+import { Rating } from "@mui/material";
 
 export function ReviewTable() {
   return (
     <List className={styles.reviewsList}>
       <ListItem className={styles.reviewsListItem}>
         <ListItemAvatar>
-          <Avatar alt="Anonymous" src="/static/images/avatar/1.jpg" />
+          <Avatar className={styles.reviewsAvatar}>
+            <NaturePeopleIcon />
+          </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Amazing!"
           secondary={<p>This is my favourite park.</p>}
         />
+        <Rating name="read-only" value={5} readOnly />
       </ListItem>
       <Divider variant="inset" component="li" />
       <ListItem>
         <ListItemAvatar>
-          <Avatar alt="Anonymous" src="/static/images/avatar/2.jpg" />
+          <Avatar className={styles.reviewsAvatar}>
+            <NaturePeopleIcon />
+          </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Breathtaking"
           secondary={<p>I highly recommend this.</p>}
         />
+        <Rating name="read-only" value={4} readOnly />
       </ListItem>
       <Divider variant="inset" component="li" />
       <ListItem>
         <ListItemAvatar>
-          <Avatar alt="Anonymous" src="/static/images/avatar/3.jpg" />
+          <Avatar className={styles.reviewsAvatar}>
+            <NaturePeopleIcon />
+          </Avatar>
         </ListItemAvatar>
         <ListItemText
           primary="Wow"
           secondary={<p>Don't forget bear spray!</p>}
         />
+        <Rating name="read-only" value={4.5} readOnly />
       </ListItem>
     </List>
   );
