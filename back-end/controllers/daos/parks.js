11c644b3d39b6911b5f6627961dda8cf9788816a
syedwaqarahmed <syedw@student.ubc.ca>
2023-06-10 17:32:07 -0700

    adding logic for db population and query

---
+++ back-end/controllers/daos/parks.js
@@ -0,0 +1,41 @@
+const { getModelForCollection } = require("../../shared/mongoose");
+
+// import data from ('../../data/allParks.json')
+// const data = require("../../data/allParks.json");
+
+const fs = require("fs");
+
+const { data } = JSON.parse(fs.readFileSync("../../data/allParks.json"));
+
+// ONLY RUN to populate local DB upon initialization of backend
+const populateLocaldatabase = async () => {
+  const model = await getModelForCollection("parksSchema");
+  for (park of data) {
+    try {
+      const obj = new model(park);
+      console.log(obj);
+      await obj.save();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
+
+const getParks = async (req) => {
+  const activities1 = "Shopping";
+  const activies2 = "Astronomy";
+  const state = "CA";
+
+  const parks = await getModelForCollection("parksSchema");
+  const query = await parks.findOne({
+    $and: [
+      { "activities.name": activities1 },
+      { "activities.name": activies2 },
+      { states: state },
+    ],
+  });
+
+  console.log(query);
+};
+
+getParks();
