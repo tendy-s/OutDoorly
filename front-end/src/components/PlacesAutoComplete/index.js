90203b1d90bb39e62176fc895e8296b63e4c7591
Tendy Su <tendysu@gmail.com>
2023-06-23 16:45:47 -0700

    updates places autocomplete

--- front-end/src/components/PlacesAutoComplete/index.js
+++ front-end/src/components/PlacesAutoComplete/index.js
@@ -1,32 +1,69 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import styles from "./places-autocomplete.module.scss";
 
-// Source: https://www.telerik.com/blogs/integrating-google-places-autocomplete-api-react-app
+// Tutorial: https://betterprogramming.pub/the-best-practice-with-google-place-autocomplete-api-on-react-939211e8b4ce
 
-const PlacesAutoComplete = ({ onChange }) => {
-  const autoCompleteRef = useRef();
-  const inputRef = useRef();
-  const options = {
-    componentRestrictions: { country: "us" },
-    fields: ["address_components", "icon", "name"],
-    types: ["(cities)"],
-  };
+let autoComplete;
+
+const loadScript = (url, callback) => {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+
+  if (script.readyState) {
+    script.onreadystatechange = function () {
+      if (script.readyState === "loaded" || script.readyState === "complete") {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {
+    script.onload = () => callback();
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+};
+
+function handleScriptLoad(updateQuery, autoCompleteRef) {
+  autoComplete = new window.google.maps.places.Autocomplete(
+    autoCompleteRef.current,
+    { types: ["(cities)"], componentRestrictions: { country: "us" } }
+  );
+  autoComplete.setFields(["address_components", "formatted_address"]);
+  autoComplete.addListener("place_changed", () =>
+    handlePlaceSelect(updateQuery)
+  );
+}
+
+async function handlePlaceSelect(updateQuery) {
+  const addressObject = autoComplete.getPlace();
+  const query = addressObject.formatted_address;
+  updateQuery(query);
+  console.log(addressObject);
+}
+
+function PlacesAutoComplete() {
+  const [query, setQuery] = useState("");
+  const autoCompleteRef = useRef(null);
+
   useEffect(() => {
-    autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-      inputRef.current,
-      options
+    console.log("key", process.env.GOOGLE_API_KEY);
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places&callback`,
+      () => handleScriptLoad(setQuery, autoCompleteRef)
     );
-    autoCompleteRef.current.addListener("place_changed", async function () {
-      const place = await autoCompleteRef.current.getPlace();
-      console.log({ place });
-      onChange(place);
-    });
   }, []);
 
   return (
-    <div>
-      <input ref={inputRef} />
+    <div className="search-location-input">
+      <input
+        ref={autoCompleteRef}
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Enter a City"
+        value={query}
+      />
     </div>
   );
-};
+}
+
 export default PlacesAutoComplete;
