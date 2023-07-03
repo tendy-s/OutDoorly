e488103b471b2c272b649c4adb66219b050d7294
syedwaqarahmed <syedw@student.ubc.ca>
2023-07-03 08:30:42 -0700

    reviews controller complete and tested on postman.

--- back-end/controllers/reviewController.js
+++ back-end/controllers/reviewController.js
@@ -1,1 +1,100 @@
-const reviewDao = require('./daos/review');
+const {
+  addReviewDAOS,
+  editReviewDAOS,
+  deleteReviewDAOS,
+} = require("./daos/review");
+
+// Create/Add User Review
+const addReview = async (req, res) => {
+  const parkId = req.params.id;
+  const reviewData = req.body;
+  const { comment, userName, userID, title, experienceRating } = req.body;
+
+  const newReview = {
+    comment,
+    createdAt: new Date(),
+    userName,
+    userID,
+    title,
+    experienceRating,
+  };
+
+  try {
+    validateInput(reviewData);
+    await addReviewDAOS(newReview, parkId);
+    res.status(201).send({ message: "Review added successfully." });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+// Update/Edit User Review
+const updateReview = async (req, res) => {
+  const parkId = req.params.id;
+  const reviewData = req.body;
+  const { comment, userName, userID, title, experienceRating } = req.body;
+  const editedReview = {
+    comment,
+    createdAt: new Date(),
+    userName,
+    userID,
+    title,
+    experienceRating,
+  };
+  try {
+    validateInput(reviewData);
+    await editReviewDAOS(editedReview, parkId);
+    res.status(200).send({ message: "Review updated successfully." });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+// Delete User Review
+const deleteReview = async (req, res) => {
+  const parkId = req.params.id;
+  const { userID } = req.body;
+
+  if (!userID || typeof userID !== "number") {
+    return res
+      .status(400)
+      .send({ error: "userID is required and must be a number." });
+  }
+
+  try {
+    await deleteReviewDAOS(userID, parkId);
+    res.status(200).send({ message: "Review deleted successfully." });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+const validateInput = ({
+  comment,
+  userName,
+  userID,
+  title,
+  experienceRating,
+}) => {
+  if (!userID || typeof userID !== "number") {
+    throw new Error("userID is required and must be a number.");
+  }
+
+  if (typeof comment !== "string") {
+    throw new Error("comment must be a string.");
+  }
+
+  if (typeof userName !== "string") {
+    throw new Error("userName must be a string.");
+  }
+
+  if (typeof title !== "string") {
+    throw new Error("title must be a string.");
+  }
+
+  if (typeof experienceRating !== "number") {
+    throw new Error("experienceRating must be a number.");
+  }
+};
+
+module.exports = { addReview, updateReview, deleteReview };
