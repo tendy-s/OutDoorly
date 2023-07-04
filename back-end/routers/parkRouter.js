16ecb60b8a674ec42223c181dc8ab4b0c844f7c7
micdong <micdong@student.ubc.ca>
2023-07-04 10:00:52 -0700

    added endpoints for images

--- back-end/routers/parkRouter.js
+++ back-end/routers/parkRouter.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const getParksStateAndActivities = require('../controllers/parksController.js');
 const getParksByDistance = require("../controllers/parksByDistanceController.js");
-// const getParkDetails = require('../controllers/parksController.js');
 const parkController = require("../controllers/parksController.js");
 const review = require("../controllers/reviewController.js");
+const imageController = require("../controllers/imagesController.js");
 
 /* root: /parks */
 
@@ -16,6 +15,11 @@ router.get("/review/:id", review.getReview);
 router.post("/review/:id", review.addReview);
 router.put("/review/:id", review.updateReview);
 router.delete("/review/:id", review.deleteReview);
+
+router.post("/images/:id", imageController.uploadImage);
+router.put("/images/:id", imageController.editImage);
+router.delete("/images/:id", imageController.deleteImage);
+router.get("/images/:id", imageController.retrieveImages);
 
 router.get("/:id", parkController.getParkDetails);
 
